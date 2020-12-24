435ec28812234f5865403e4dd1fcf9bc2e64eb66
Steven Goldburg <59116258+goldyvb@users.noreply.github.com>
2020-12-24 13:43:55 -0500

    Add files via upload

---
+++ Hexagon.java
@@ -0,0 +1,41 @@
+
+public class Hexagon 
+{
+	private int x;
+	private int y;
+	private boolean isWhite;
+	
+	public Hexagon(int xinit, int yinit)
+	{
+		x = xinit;
+		y = yinit;
+		isWhite = true;
+	}
+	
+	public void changeColor()
+	{
+		isWhite = !isWhite;
+	}
+	
+	public int getX()
+	{
+		return x;
+	}
+	
+	public int getY()
+	{
+		return y;
+	}
+	
+	public boolean getColor()
+	{
+		return isWhite;
+	}
+	
+	public String toString()
+	{
+		return x+" " +y;
+	}
+	
+	
+}
